5b340b00c75801eb4e30a7c436257041ff13de98
Harishvemulakonda <harishvemulakonda1992@gmail.com>
2022-10-22 22:51:41 +0530

    ok first

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
 
 
-$(document).ready(function () {
-    //Owl
-    $('.hero-slider').owlCarousel({
-        loop: true,
-        margin: 0,
-        items: 1,
-        dots: false,
-        navText: ['PREV', 'NEXT'],
-        smartSpeed: 1000,
-        autoplay: true,
-        autoplayTimeout: 7000,
-        responsive: {
-            0: {
-                nav: false,
-            },
-            768: {
-                nav: true,
-            }
-        }
-    })
+// $(document).ready(function () {
+//     //Owl
+//     $('.carousel-inner').owlCarousel({
+//         loop: true,
+//         margin: 0,
+//         items: 1,
+//         dots: false,
+//         navText: ['PREV', 'NEXT'],
+//         smartSpeed: 1000,
+//         autoplay: true,
+//         autoplayTimeout: 7000,
+//         responsive: {
+//             0: {
+//                 nav: false,
+//             },
+//             768: {
+//                 nav: true,
+//             }
+//         }
+//     })
 
     $('#projects-slider').owlCarousel({
         loop: true,
